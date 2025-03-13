6a25225b26b444aa539b14387fab71183873d01a
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2025-03-13 00:08:58 -0300

    Unit tests for the wrapper class

---
+++ test_wrapper.py
@@ -0,0 +1,25 @@
+import time
+import unittest
+from python_wrapper import EventBus
+
+class TestEventBus(unittest.TestCase):
+    def setUp(self):
+        self.bus = EventBus()
+
+    def test_subscribe_and_publish(self):
+        self.callback_called = False
+
+        def callback(arg1, arg2):
+            self.callback_called = True
+            self.assertEqual(arg1, "Hello")
+            self.assertEqual(arg2, "World")
+
+        self.bus.subscribe("test_event", callback)
+        self.bus.publish("test_event", ["Hello", "World"])
+        
+        time.sleep(0.0001)
+
+        self.assertTrue(self.callback_called)
+
+if __name__ == "__main__":
+    unittest.main()
