dbf2a9ff73e181f26f52e1ca80ea3bf258b63c7c
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2025-03-13 00:08:32 -0300

    Testing the final versions

--- benchmark.py
+++ benchmark.py
@@ -1,6 +1,7 @@
 import time
 import concurrent.futures
 from python_multithread import MultiThreadEventBus
+from python_wrapper import EventBus as RustEventBus
 from python_sync import EventBus
 
 EVENTS = 5000
@@ -41,7 +42,25 @@ def benchmark_threadpool(event_bus, events=EVENTS, subscribers=SUBSCRIBERS):
     elapsed = time.time() - start_time
     print(f"ThreadPool EventBus: {events} events in {elapsed:.4f} sec ({events/elapsed:.2f} events/sec)")
 
+# Rust Benchmark
+def rust_event_bus_benchmark(event_bus: RustEventBus, events=EVENTS, subscribers=SUBSCRIBERS):
 
+    # Register multiple subscribers
+    for _ in range(subscribers):
+        event_bus.subscribe("test_event", cpu_bound_task)
+
+    # Start the benchmark
+    start_time = time.perf_counter()
+
+    for _ in range(events):
+        event_bus.publish("test_event", ["data"])
+
+    end_time = time.perf_counter()
+    duration = end_time - start_time
+    events_per_second = events / duration
+
+    print(f"Rust EventBus: {events} events in {duration:.4f} sec ({events_per_second:.2f} events/sec)")
+    
 # Run all benchmarks
 if __name__ == "__main__":
     print("\n[1] Testing Sync EventBus")
@@ -51,3 +70,7 @@ if __name__ == "__main__":
     print(f"\n[2] Testing ThreadPool EventBus with {MAX_WORKERS} worker(s)")
     threadpool_bus = MultiThreadEventBus()
     benchmark_threadpool(threadpool_bus)
+    
+    print(f"\n[2] Testing Rust EventBus")
+    rust_event_bus = RustEventBus()
+    rust_event_bus_benchmark(rust_event_bus)
